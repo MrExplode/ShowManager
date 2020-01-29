ba30e4c6e869836168336c6c0655920eeef07451
MrExplode <pjanos@outlook.hu>
2020-01-29 14:07:44 +0100

    Memory checks

--- src/main/java/me/mrexplode/timecode/Bootstrap.java
+++ src/main/java/me/mrexplode/timecode/Bootstrap.java
@@ -1,18 +1,19 @@
 package me.mrexplode.timecode;
 
-import java.awt.EventQueue;
+import java.lang.management.ManagementFactory;
 import java.net.SocketException;
+
+import javax.swing.JOptionPane;
 
 public class Bootstrap {
 
-    public static void main(String[] args) {
-        System.out.println("all: " + Runtime.getRuntime().maxMemory() / 1000000);
-        try {
-            MainGUI gui = new MainGUI();
-            gui.setVisible(true);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+    @SuppressWarnings("restriction")
+	public static void main(String[] args) {
+    	long memorySize = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalPhysicalMemorySize();
+    	long freeSysMem = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getFreePhysicalMemorySize();
+    	if (freeSysMem < 1000000000) {
+    		JOptionPane.showConfirmDialog(null, "Less than 1 GB free memory! Please free the memory if you wish to use this program", "Timecode Generator", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE, null);
+    	}
         /*
         if (args.length == 0) {
             System.out.println("all: " + Runtime.getRuntime().maxMemory() / 1000000);
