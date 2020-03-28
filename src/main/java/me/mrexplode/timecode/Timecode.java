54f15b451e3498aec64954b98c99582b6f0d6051
MrExplode <pjanos@outlook.hu>
2020-03-28 21:35:31 +0100

    Sortable timecode

--- src/main/java/me/mrexplode/timecode/Timecode.java
+++ src/main/java/me/mrexplode/timecode/Timecode.java
@@ -6,7 +6,7 @@ package me.mrexplode.timecode;
  * @author <a href="https://mrexplode.github.io">MrExplode</a>
  *
  */
-public class Timecode {
+public class Timecode implements Comparable<Timecode> {
     
     private int hour;
     private int min;
@@ -60,5 +60,83 @@ public class Timecode {
         this.frame = frame;
     }
     
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + frame;
+        result = prime * result + hour;
+        result = prime * result + min;
+        result = prime * result + sec;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Timecode other = (Timecode) obj;
+        if (frame != other.getFrame())
+            return false;
+        if (hour != other.getHour())
+            return false;
+        if (min != other.getMin())
+            return false;
+        if (sec != other.getSec())
+            return false;
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec) + "/" + (frame < 10 ? "0" + frame : frame); 
+    }
+
+
+    @Override
+    public int compareTo(Timecode tc) {
+        if (tc == null) {
+            return 1;
+        }
+        if (this.equals(tc)) {
+            return 0;
+        }
+        
+        if (getHour() >= tc.getHour()) {
+            if (getHour() == tc.getHour()) {
+                if (getMin() >= tc.getMin()) {
+                    if (getMin() == tc.getMin()) {
+                        if (getSec() >= tc.getSec()) {
+                            if (getSec() == tc.getSec()) {
+                                if (getFrame() >= tc.getFrame()) {
+                                    if (getFrame() == tc.getFrame()) {
+                                        //should not happen
+                                        return 0;
+                                    }
+                                    return 1;
+                                } else {
+                                    return -1;
+                                }
+                            }
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+                    }
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 
 }
