fe52bdd615ac5f61c9b14f568530653877ef2189
MrExplode <pjanos@outlook.hu>
2020-04-23 16:33:40 +0200

    Changed to OSHI

--- src/main/java/me/mrexplode/timecode/Bootstrap.java
+++ src/main/java/me/mrexplode/timecode/Bootstrap.java
@@ -1,21 +1,35 @@
 package me.mrexplode.timecode;
 
+import java.awt.Desktop;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.SocketException;
+import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.event.HyperlinkEvent;
 
 import me.mrexplode.timecode.gui.ClientGUI;
 import me.mrexplode.timecode.gui.ServerGUI;
+import oshi.SystemInfo;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HardwareAbstractionLayer;
 
 public class Bootstrap {
 
     public static void main(String[] args) {
+        String ver = ManagementFactory.getRuntimeMXBean().getVmVersion();
+        if (!ver.startsWith("14")) {
+            showVersionError(ver);
+            return;
+        }
         ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(args));
         if (arguments.contains("--client")) {
             EventQueue.invokeLater(() -> {
@@ -23,7 +37,7 @@ public class Bootstrap {
                 gui.setVisible(true);
             });
         } else {
-            if (arguments.contains("withRAM") || arguments.contains("debugStart")) {
+            if (arguments.contains("--withRAM") || arguments.contains("--debugStart")) {
                 EventQueue.invokeLater(() ->{
                     try {
                         ServerGUI gui = new ServerGUI();
@@ -33,14 +47,17 @@ public class Bootstrap {
                     }
                 });
             } else {
-                long freeSysMem = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getFreePhysicalMemorySize();
+                SystemInfo si = new SystemInfo();
+                HardwareAbstractionLayer h = si.getHardware();
+                GlobalMemory globalMem = h.getMemory();
+                long freeSysMem = globalMem.getAvailable();
                 if (freeSysMem < 3000000000L) {
                     JOptionPane.showConfirmDialog(null, "Less than 3 GB free memory! Please free the memory if you wish to use this program", "Timecode Generator", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE, null);
                     return;
                 } else {
                     try {
                         //JOptionPane.showConfirmDialog(null, "Starting main app...", "Timecode Generator", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
-                        Runtime.getRuntime().exec(new String [] {"java" ,"-Xmx3G", "-jar", URLDecoder.decode(Bootstrap.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6), "UTF-8"), "withRAM"});
+                        Runtime.getRuntime().exec(new String [] {"java" ,"-Xmx3G", "-jar", URLDecoder.decode(Bootstrap.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6), "UTF-8"), "--withRAM"});
                     } catch (IOException e) {
                         JOptionPane.showConfirmDialog(null, "Failed to start the process!\n " + e.getMessage(), "Timecode Generator", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE, null);
                         e.printStackTrace();
@@ -49,6 +66,29 @@ public class Bootstrap {
                 }
             }
         }
+    }
+    
+    private static void showVersionError(String currentVer) {
+        JLabel label = new JLabel();
+        Font font = label.getFont();
+        
+        StringBuffer style = new StringBuffer("font-family:" + font.getFamily() + ";");
+        style.append("font-weight:" + (font.isBold() ? "bold" : "normal") + ";");
+        style.append("font-size:" + font.getSize() + "pt;");
+        
+        JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">Required Java version: JDK 14<br>Current: " + currentVer + "<br><a href=\"https://adoptopenjdk.net/index.html?variant=openjdk14&jvmVariant=hotspot\">Recommended download</a>");
+        ep.setEditable(false);
+        ep.setBackground(label.getBackground());
+        ep.addHyperlinkListener(e -> {
+            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+                try {
+                    Desktop.getDesktop().browse(e.getURL().toURI());
+                } catch (IOException | URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        JOptionPane.showConfirmDialog(null, ep, "Timecode Generator", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE, null);
     }
     
 }
