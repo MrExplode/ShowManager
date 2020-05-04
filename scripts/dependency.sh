0538049d52a58cec7c1ae521442e75622cf1aece
MrExplode <pjanos@outlook.hu>
2020-05-04 13:26:33 +0200

    Travis script change

--- scripts/dependency.sh
+++ scripts/dependency.sh
@@ -2,3 +2,7 @@ cd $HOME
 git clone --branch=master https://${GITHUB_TOKEN}@github.com/MrExplode/ltc4j ltc
 cd ltc
 mvn install
+cd $HOME
+git clone --branch=master https://${GITHUB_TOKEN}@https://github.com/hoijui/JavaOSC osc
+cd osc
+mvn install
