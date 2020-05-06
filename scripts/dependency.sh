ff3bc8119944e06cc71f40a4d8d0907124fceba8
MrExplode <pjanos@outlook.hu>
2020-05-06 23:38:38 +0200

    fuck this shit
    Am I blind, tired or just retarded? pls work

--- scripts/dependency.sh
+++ scripts/dependency.sh
@@ -3,6 +3,6 @@ git clone --branch=master https://${GITHUB_TOKEN}@github.com/MrExplode/ltc4j ltc
 cd ltc
 mvn install
 cd $HOME
-git clone --branch=master https://${GITHUB_TOKEN}@https://github.com/hoijui/JavaOSC osc
+git clone --branch=master https://${GITHUB_TOKEN}@github.com/hoijui/JavaOSC osc
 cd osc
 mvn install
