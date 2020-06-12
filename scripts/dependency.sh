86acc724ddfe662c905a845631201843d1dd0ed1
MrExplode <pjanos@outlook.hu>
2020-06-12 12:05:06 +0200

    Removed dependency from script
    They finally released the 0.7

--- scripts/dependency.sh
+++ scripts/dependency.sh
@@ -2,7 +2,3 @@ cd "$HOME"
 git clone --branch=master https://"${GITHUB_TOKEN}"@github.com/MrExplode/ltc4j ltc
 cd ltc
 mvn install
-cd "$HOME"
-git clone --branch=master https://"${GITHUB_TOKEN}"@github.com/hoijui/JavaOSC osc
-cd osc
-mvn install -Dmaven.test.skip=true -Dfindbugs.skip=true
