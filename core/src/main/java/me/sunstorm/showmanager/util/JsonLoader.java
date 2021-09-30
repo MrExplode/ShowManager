aedb3d15dabe4847fec6c9372e1fd6898d1cb691
MrExplode <pjanos@outlook.hu>
2021-09-30 23:58:01 +0200

    Expose exceptions in JsonLoader

--- core/src/main/java/me/sunstorm/showmanager/util/JsonLoader.java
+++ core/src/main/java/me/sunstorm/showmanager/util/JsonLoader.java
@@ -1,9 +1,12 @@
 package me.sunstorm.showmanager.util;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import me.sunstorm.showmanager.Constants;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -12,53 +15,48 @@ import java.nio.charset.StandardCharsets;
 @UtilityClass
 public class JsonLoader {
 
-    public <T> T loadOrDefault(String name, Class<T> configType) {
+    public JsonElement getJsonElement(File file) throws FileNotFoundException {
+        return JsonParser.parseReader(new InputStreamReader(new FileInputStream(file)));
+    }
+
+    public <T> T loadOrDefault(String name, Class<T> configType) throws IOException {
         return loadOrDefault(new File(Constants.BASE_DIRECTORY, name), configType);
     }
 
-    public <T> T loadOrDefault(File configFile, Class<T> configType) {
+    public <T> T loadOrDefault(@NotNull File configFile, Class<T> configType) throws IOException {
         if (configFile.exists())
             return loadConfig(configFile, configType);
         T config;
         try {
             config = configType.getConstructor().newInstance();
         } catch (ReflectiveOperationException e) {
-            log.error("Failed to create instance for " + configType.getSimpleName(), e);
-            return null;
+            throw new RuntimeException(e);
         }
-        try {
-            if (!new File("config").exists())
-                new File("config").mkdirs();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(configFile), StandardCharsets.UTF_8));
-            Constants.GSON.toJson(config, writer);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            log.error("Failed to write default config for " + configType.getSimpleName(), e);
-        }
+        if (!new File("config").exists())
+            new File("config").mkdirs();
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(configFile), StandardCharsets.UTF_8));
+        Constants.GSON.toJson(config, writer);
+        writer.flush();
+        writer.close();
         return config;
     }
 
-    @SneakyThrows
-    public <T> T loadConfig(String name, Class<T> configType) {
+    public <T> T loadConfig(String name, Class<T> configType) throws IOException {
         return loadConfig(new File(Constants.BASE_DIRECTORY, name), configType);
     }
 
-    @SneakyThrows
-    public <T> T loadConfig(File file, Class<T> configType) {
+    public <T> T loadConfig(File file, Class<T> configType) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
         T object = Constants.GSON.fromJson(reader, configType);
         reader.close();
         return object;
     }
 
-    @SneakyThrows
-    public void saveConfig(String name, Object config) {
+    public void saveConfig(String name, Object config) throws IOException {
         saveConfig(new File(Constants.BASE_DIRECTORY, name), config);
     }
 
-    @SneakyThrows
-    public void saveConfig(File file, Object config) {
+    public void saveConfig(File file, Object config) throws IOException {
         PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8));
         Constants.GSON.toJson(config, writer);
         writer.flush();
