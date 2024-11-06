1a5a4593c1790b5592aa9ed121d3c60b04552795
MrExplode <me@sunstorm.rocks>
2024-11-06 14:30:22 +0100

    fix(web): limit scheduler height

--- webapp/src/lib/components/scheduler/SchedulerPanel.svelte
+++ webapp/src/lib/components/scheduler/SchedulerPanel.svelte
@@ -5,6 +5,7 @@
     import { events } from '$lib/data/scheduler'
     import { columns } from '@/scheduler/event_columns'
     import CreateEventDialog from './CreateEventDialog.svelte'
+    import { ScrollArea } from '$components/ui/scroll-area'
 </script>
 
 <Card.Root class="m-2 self-start">
@@ -14,6 +15,8 @@
         <CreateEventDialog class="ml-6" />
     </Card.Header>
     <Card.Content>
-        <DataTable data={$events} {columns} />
+        <ScrollArea class="h-64">
+            <DataTable data={$events} {columns} />
+        </ScrollArea>
     </Card.Content>
 </Card.Root>
