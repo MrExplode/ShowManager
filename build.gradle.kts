459d09b15de4911e762e75217ce3a9e052e8ad29
MrExplode <me@sunstorm.rocks>
2024-11-11 13:42:31 +0100

    feat: gradle task for webapp build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
+import org.apache.tools.ant.taskdefs.condition.Os
 
 plugins {
     java
@@ -46,5 +47,17 @@ tasks.jar {
 
 tasks.assemble {
     dependsOn(tasks.shadowJar)
+}
+
+tasks.register("build-webapp", Exec::class.java) {
+    workingDir = layout.projectDirectory.dir("webapp").asFile
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        commandLine("cmd.exe", "/c", "pnpm", "run", "build")
+    } else {
+        commandLine("bash", "-c", "pnpm", "run", "build")
+    }
+    doLast {
+        println("Webapp build done!")
+    }
 }
 
