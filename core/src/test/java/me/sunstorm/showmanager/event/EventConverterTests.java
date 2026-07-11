5ca61ce9a1977793ee24a10a392d0c0ba619c8a6
MrExplode <me@sunstorm.rocks>
2026-07-11 23:27:27 +0200

    test(core): align serialization tests with cluster whitelist

--- core/src/test/java/me/sunstorm/showmanager/event/EventConverterTests.java
+++ core/src/test/java/me/sunstorm/showmanager/event/EventConverterTests.java
@@ -1,35 +1,39 @@
 package me.sunstorm.showmanager.event;
 
-import me.sunstorm.showmanager.eventsystem.events.audio.AudioVolumeChangeEvent;
 import me.sunstorm.showmanager.cluster.serial.EventConverter;
 import me.sunstorm.showmanager.cluster.serial.EventWrapper;
+import me.sunstorm.showmanager.eventsystem.events.time.TimecodeChangeEvent;
+import me.sunstorm.showmanager.eventsystem.events.transport.TransportCommandEvent;
+import me.sunstorm.showmanager.util.Timecode;
 import org.junit.jupiter.api.Test;
-
-import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class EventConverterTests {
 
+    private final EventConverter converter = new EventConverter();
+
     @Test
-    void testEncode() {
-        EventWrapper wrapper = new EventWrapper(5, false, "node1", new AudioVolumeChangeEvent(15));
-        String excepted = "{\"id\":5,\"async\":false,\"origin\":\"node1\",\"event\":{\"volume\":15}}";
-        EventConverter converter = new EventConverter();
-        assertThat(converter.encode(wrapper)).isEqualTo(excepted.getBytes(StandardCharsets.UTF_8));
+    void roundTripsTimecodeChange() {
+        EventWrapper wrapper = new EventWrapper(11, false, "node1", new TimecodeChangeEvent(new Timecode(0, 1, 30, 0)));
+        EventWrapper result = converter.decode(converter.encode(wrapper));
+        assertThat(result.id()).isEqualTo(11);
+        assertThat(result.async()).isFalse();
+        assertThat(result.origin()).isEqualTo("node1");
+        assertThat(result.event()).isInstanceOf(TimecodeChangeEvent.class);
+        assertThat(((TimecodeChangeEvent) result.event()).getTime()).isEqualTo(new Timecode(0, 1, 30, 0));
     }
 
     @Test
-    void testDecode() {
-        String input = "{\"id\":5,\"async\":false,\"origin\":\"node1\",\"event\":{\"volume\":15}}";
-        EventWrapper excepted = new EventWrapper(5, false, "node1", new AudioVolumeChangeEvent(15));
-        EventConverter converter = new EventConverter();
-        assertThat(converter.decode(input.getBytes(StandardCharsets.UTF_8))).satisfies(w -> {
-            assertThat(w.id()).as("event id").isEqualTo(excepted.id());
-            assertThat(w.async()).isEqualTo(excepted.async());
-            assertThat(w.origin()).isEqualTo(excepted.origin());
-            assertThat(w.event()).isNotNull().isInstanceOf(AudioVolumeChangeEvent.class);
-            assertThat(((AudioVolumeChangeEvent) w.event()).getVolume()).isEqualTo(15);
-        });
+    void roundTripsTransportCommandWithTime() {
+        EventWrapper wrapper = new EventWrapper(5, false, "node2",
+                new TransportCommandEvent(TransportCommandEvent.Action.SET, new Timecode(0, 0, 42, 0)));
+        EventWrapper result = converter.decode(converter.encode(wrapper));
+        assertThat(result.id()).isEqualTo(5);
+        assertThat(result.origin()).isEqualTo("node2");
+        assertThat(result.event()).isInstanceOf(TransportCommandEvent.class);
+        TransportCommandEvent command = (TransportCommandEvent) result.event();
+        assertThat(command.getAction()).isEqualTo(TransportCommandEvent.Action.SET);
+        assertThat(command.getTime()).isEqualTo(new Timecode(0, 0, 42, 0));
     }
 }
