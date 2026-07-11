bda2c3c293933cfb156b38f655af4309ebf6c379
MrExplode <me@sunstorm.rocks>
2026-07-11 15:17:11 +0200

    refactor(core): remove dead packet events

--- core/src/main/java/me/sunstorm/showmanager/eventsystem/events/redis/PacketReceiveEvent.java
+++
@@ -1,6 +0,0 @@
-package me.sunstorm.showmanager.eventsystem.events.redis;
-
-import me.sunstorm.showmanager.eventsystem.events.Event;
-
-public class PacketReceiveEvent extends Event {
-}
--- core/src/main/java/me/sunstorm/showmanager/eventsystem/events/redis/PacketSendEvent.java
+++
@@ -1,6 +0,0 @@
-package me.sunstorm.showmanager.eventsystem.events.redis;
-
-import me.sunstorm.showmanager.eventsystem.events.CancellableEvent;
-
-public class PacketSendEvent extends CancellableEvent {
-}
