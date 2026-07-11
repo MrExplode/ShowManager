711240732c9c44da8795d882f2982e00996997e0
MrExplode <me@sunstorm.rocks>
2026-07-11 17:02:03 +0200

    feat(core): add cluster config

---
+++ core/src/main/java/me/sunstorm/showmanager/settings/config/ClusterConfig.java
@@ -0,0 +1,42 @@
+package me.sunstorm.showmanager.settings.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClusterConfig {
+    private boolean enabled = false;
+    private String clusterName = "showmanager";
+    private String nodeName = "";
+    private String bindAddress = "";
+    private int port = 7800;
+    private boolean useMulticast = true;
+    private List<String> seedNodes = new ArrayList<>();
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public String getBindAddress() {
+        return bindAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public boolean isUseMulticast() {
+        return useMulticast;
+    }
+
+    public List<String> getSeedNodes() {
+        return seedNodes;
+    }
+}
--- core/src/main/java/me/sunstorm/showmanager/settings/config/Config.java
+++ core/src/main/java/me/sunstorm/showmanager/settings/config/Config.java
@@ -3,6 +3,7 @@ package me.sunstorm.showmanager.settings.config;
 public class Config {
     private int framerate = 25;
     private LtcConfig ltcConfig = new LtcConfig();
+    private ClusterConfig clusterConfig = new ClusterConfig();
 
     public int getFramerate() {
         return framerate;
@@ -10,5 +11,9 @@ public class Config {
 
     public LtcConfig getLtcConfig() {
         return ltcConfig;
+    }
+
+    public ClusterConfig getClusterConfig() {
+        return clusterConfig;
     }
 }
